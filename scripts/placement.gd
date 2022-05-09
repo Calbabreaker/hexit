291ee893ab3f47472218a0d78b42b426f2735490
Calbabreaker <calbabreaker@gmail.com>
2022-05-09 22:57:07 +0930

    fix script thing

--- scripts/placement.gd
+++ scripts/placement.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-const invalid_color := Color(0xff0000ff)
-
 export var piece_prefab: PackedScene
 export var score_label_path: NodePath
 onready var score_label = get_node(score_label_path)
@@ -10,28 +8,26 @@ onready var tween := $Tween
 var grid := {}
 var score: int
 var ghost_piece: Node
+var mouse_hex_pos: Vector2
 
 func _ready() -> void:
 	set_ghost_piece()
 	set_score(0)
+	
+func _process(delta: float) -> void:
+	# Make the ghost piece follow the mouse
+	mouse_hex_pos = Piece.pixel_to_hex(get_local_mouse_position())
+	ghost_piece.position = Piece.hex_to_pixel(mouse_hex_pos)
+	ghost_piece.modulate.r = 10 if grid.has(mouse_hex_pos) else 1 # Redden sprite when piece already exists there
 
 func _input(event: InputEvent) -> void:
-	# If the mouse moved or clicked
-	if event is InputEventMouse:
-		var hex_pos = Piece.pixel_to_hex(get_local_mouse_position())
-			
-		# If the mouse is pressed and a piece hasn't been placed at the position yet
-		if event.is_action_pressed("ui_click") && !grid.has(hex_pos):
-			# Reset the ghost piece and put it inside the grid
-			tween.remove(ghost_piece)
-			ghost_piece.modulate = Color(0xffffffff)
-			grid[hex_pos] = ghost_piece
-			set_ghost_piece()
-		
-		# Make the ghost piece follow the mouse
-		# Set down here to ensure the newly created ghost piece is at the mouse
-		ghost_piece.position = Piece.hex_to_pixel(hex_pos)
-		ghost_piece.modulate = invalid_color if grid.has(hex_pos) else Color.white
+	# If the mouse is pressed and a piece hasn't been placed at the position yet
+	if event.is_action_pressed("ui_click") && !grid.has(mouse_hex_pos):
+		# Reset the ghost piece and put it inside the grid
+		tween.remove(ghost_piece)
+		ghost_piece.modulate.a = 1
+		grid[mouse_hex_pos] = ghost_piece
+		set_ghost_piece()
 			
 func set_ghost_piece():
 	ghost_piece = piece_prefab.instance()
