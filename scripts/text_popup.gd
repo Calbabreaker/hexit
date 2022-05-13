5e9ee6b8a7e6516979448483b0875106244d7cc3
Calbabreaker <calbabreaker@gmail.com>
2022-05-13 20:49:32 +0930

    fix weird can't click bug

--- scripts/text_popup.gd
+++ scripts/text_popup.gd
@@ -11,3 +11,7 @@ func init(text: String, p_position: Vector2) -> void:
 	tween.interpolate_property(self, "modulate:a", 1, 0, 1)
 	tween.interpolate_property(self, "rect_position:y", null, rect_position.y - 30, 1)
 	tween.start()
+
+# Destroy itself when animation finsihed
+func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
+	queue_free()
